b0f918ce9a3c97d935bda1fab5c332d9bfe7b394
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-04 00:44:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,12 +3,12 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |key, value|
     value.each do |language, properties|
-       new_hash[language] = properties
-       if new_hash[language][:style] 
-        new_hash[language][:style] << key
-      else
+      if new_hash[language] == nil
+        new_hash[language] = properties
         new_hash[language][:style] = [key]
-      end  
+      else
+        new_hash[language][:style] << key
+      end
     end
   end  
   binding.pry
