36e58b8ab7cc84652c90e739004099894decdf81
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-06-04 00:25:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,9 @@
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |key, value|
-    new_hash[key] = value
-  end
+    value.each do |language, properties|
+       new_hash[language] = properties
+    end
+  end  
   new_hash
 end
